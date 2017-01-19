2fe4ae70f0856a7443713c6722ffa5133a24d42c
Jen Lampton <jen@jeneration.com>
2017-01-19 15:21:28 -0800

    Add flag module.

---
+++ www/modules/contrib/flag/README.md
@@ -0,0 +1,40 @@
+Flag
+====
+
+Flag module allows you to add flags to nodes, users or any other type
+of entity. The flags can be per-user or global (per-entity).
+
+Possible uses include bookmarks, marking important, friends, or flag
+as offensive. With extensive views integration, you can create custom
+lists of popular content or keep tabs on important content.
+
+Installation
+------------
+
+- Install this module using the official Backdrop CMS instructions at
+  https://backdropcms.org/guide/modules
+
+License
+-------
+
+This project is GPL v2 software. See the LICENSE.txt file in this directory for
+complete text.
+
+Current Maintainers
+-------------------
+
+- John Romine (https://github.com/jromine/)
+
+Credits
+-------
+
+This module is based on the Drupal module Flag-7.x-3.x
+
+Project page: http://drupal.org/project/flag.
+
+Drupal Maintainers:
+- Joachim Noreiko (https://www.drupal.org/u/joachim)
+- Tess (https://www.drupal.org/u/socketwench)
+- Shabana Navas (https://www.drupal.org/u/shabana-blackborder)
+- Wolfgang Ziegler (https://www.drupal.org/u/fago)
+- Nate Haug (https://www.drupal.org/u/quicksketch)
---
+++ www/modules/contrib/flag/js/flag-admin.js
@@ -0,0 +1,64 @@
+(function ($) {
+
+/**
+ * Behavior to disable the "unflag" option if "flag" is not available.
+ */
+Backdrop.behaviors.flagRoles = {};
+Backdrop.behaviors.flagRoles.attach = function(context) {
+  $('#flag-roles input.flag-access', context).change(function() {
+    var unflagCheckbox = $(this).parents('tr:first').find('input.unflag-access').get(0);
+    if (this.checked) {
+      // If "flag" is available, restore the state of the "unflag" checkbox.
+      unflagCheckbox.disabled = false;
+      if (typeof(unflagCheckbox.previousFlagState) != 'undefined') {
+        unflagCheckbox.checked = unflagCheckbox.previousFlagState;
+      }
+      else {
+        unflagCheckbox.checked = true;
+      }
+    }
+    else {
+      // Remember if the "unflag" option was checked or unchecked, then disable.
+      unflagCheckbox.previousFlagState = unflagCheckbox.checked;
+      unflagCheckbox.disabled = true;
+      unflagCheckbox.checked = false;
+    }
+  });
+
+  $('#flag-roles input.unflag-access', context).change(function() {
+    if ($(this).parents('table:first').find('input.unflag-access:enabled:not(:checked)').size() == 0) {
+      $('div.form-item-unflag-denied-text').slideUp();
+    }
+    else {
+      $('div.form-item-unflag-denied-text').slideDown();
+    }
+  });
+
+  // Hide the link options by default if needed.
+  if ($('#flag-roles input.unflag-access:enabled:not(:checked)').size() == 0) {
+    $('div.form-item-unflag-denied-text').css('display', 'none');
+  }
+};
+
+/**
+ * Vertical tabs integration.
+ */
+Backdrop.behaviors.flagSummary = {};
+
+Backdrop.behaviors.flagSummary.attach = function (context) {
+  $('fieldset.flag-fieldset', context).backdropSetSummary(function(context) {
+    var flags = [];
+    $('input:checkbox:checked', context).each(function() {
+      flags.push(this.title);
+    });
+
+    if (flags.length) {
+      return flags.join(', ');
+    }
+    else {
+      return Backdrop.t('No flags');
+    }
+  });
+};
+
+})(jQuery);
