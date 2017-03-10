25d446a06fd0d0d1a5127d52b49c5a397a569423
Jen Lampton <jen@jeneration.com>
2017-03-09 21:56:06 -0800

    Core update.

--- www/core/misc/tabledrag.js
+++ www/core/misc/tabledrag.js
@@ -121,8 +121,8 @@ Backdrop.tableDrag = function (table, tableSettings) {
   // as event handlers do not have direct access to the tableDrag object.
   $(document).on('touchmove', function (event) { return self.dragRow(event.originalEvent.touches[0], self); });
   $(document).on('touchend', function (event) { return self.dropRow(event.originalEvent.touches[0], self); });
-  $(document).on('mousemove', function (event) { return self.dragRow(event, self); });
-  $(document).on('mouseup', function (event) { return self.dropRow(event, self); });
+  $(document).on('mousemove pointermove', function (event) { return self.dragRow(event, self); });
+  $(document).on('mouseup pointerup', function (event) { return self.dropRow(event, self); });
 };
 
 /**
@@ -623,20 +623,12 @@ Backdrop.tableDrag.prototype.dropRow = function (event, self) {
  * Get the coordinates from the event (allowing for browser differences).
  */
 Backdrop.tableDrag.prototype.pointerCoords = function (event) {
-  // Complete support for pointer events was only introduced to jQuery in
-  // version 1.11.1; between versions 1.7 and 1.11.0 pointer events have the
-  // clientX and clientY properties undefined. In those cases, the properties
-  // must be retrieved from the event.originalEvent object instead.
-  var clientX = event.clientX || event.originalEvent.clientX;
-  var clientY = event.clientY || event.originalEvent.clientY;
-
   if (event.pageX || event.pageY) {
     return { x: event.pageX, y: event.pageY };
   }
-
   return {
-    x: clientX + document.body.scrollLeft - document.body.clientLeft,
-    y: clientY + document.body.scrollTop - document.body.clientTop
+    x: event.clientX + document.body.scrollLeft - document.body.clientLeft,
+    y: event.clientY + document.body.scrollTop  - document.body.clientTop
   };
 };
 
@@ -896,7 +888,7 @@ Backdrop.tableDrag.prototype.setScroll = function (scrollAmount) {
 
   this.scrollInterval = setInterval(function () {
     // Update the scroll values stored in the object.
-    self.checkScroll(self.currentpointerCoords.y);
+    self.checkScroll(self.currentPointerCoords.y);
     var aboveTable = self.scrollY > self.table.topY;
     var belowTable = self.scrollY + self.windowHeight < self.table.bottomY;
     if (scrollAmount > 0 && belowTable || scrollAmount < 0 && aboveTable) {
