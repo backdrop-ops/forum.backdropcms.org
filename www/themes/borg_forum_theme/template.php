8cba040301d792cdccbb0d4cba481b34ebb5fdba
Jen Lampton <jen@jeneration.com>
2018-01-06 00:14:24 -0800

    Update forum theme to account for changes in latest base theme.

--- www/themes/borg_forum_theme/template.php
+++ www/themes/borg_forum_theme/template.php
@@ -8,22 +8,6 @@
  * Preprocess Functions
  ******************************************************************************/
 
-/**
- * Prepares variables for node template.
- * @see node.tpl.php
- */
-function borg_forum_theme_preprocess_node(&$variables) {
-  $key = array_search('inline', $variables['content']['links']['#attributes']['class']);
-  unset($variables['content']['links']['#attributes']['class'][$key]);
-}
-
-/**
- * Prepares variables for comment template.
- * @see comment.tpl.php
- */
-function borg_forum_theme_preprocess_comment(&$variables) {
-  $variables['submitted'] = str_replace('Submitted by', 'Comment from', $variables['submitted']);
-}
 
 /*******************************************************************************
  * Theme function Overrides
