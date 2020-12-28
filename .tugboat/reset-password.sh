28db2b773b0a752cca89c34e489376b1733c6c46
BWPanda <peter@panda.id.au>
2020-12-28 20:29:29 +1100

    Add bootstrap code.

--- .tugboat/reset-password.sh
+++ .tugboat/reset-password.sh
@@ -4,7 +4,16 @@
  * Reset User 1's password.
  */
 
+// Bootstrap Backdrop.
+define('BACKDROP_ROOT', getenv('TUGBOAT_ROOT') . '/www');
+require_once BACKDROP_ROOT . '/core/includes/bootstrap.inc';
+backdrop_bootstrap(BACKDROP_BOOTSTRAP_FULL);
+
+// Reset password.
 $password = file_get_contents(getenv('TUGBOAT_ROOT') . '/.tugboat/password');
 $user = user_load(1);
 $user->pass = $password;
 user_save($user);
+
+// Flush caches.
+backdrop_flush_all_caches();
